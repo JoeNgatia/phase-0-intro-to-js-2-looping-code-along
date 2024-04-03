9ba6a096197e38bbe8d797bbe9881028847c53c2
joengatia <ngatiajoe2@gmail.com>
2024-04-04 01:41:26 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// index.js
+
+function writeCards(namesArray) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    if (number < 0) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+module.exports.writeCards = writeCards;
+module.exports.countDown = countDown;
+
